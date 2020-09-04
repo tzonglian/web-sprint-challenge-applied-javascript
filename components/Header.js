005c1cca75155eb39e09fec6c5222962f7c95b89
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-09-04 11:19:40 -0400

    Header.js Complete

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+//Q - Should 'Header' be capitalized??? (default given by sprint)
+function Header() {
+
+    // Instatiate elements
+    const headerGroup = document.createElement('div')
+    const headerDate = document.createElement('span')
+    const headerTitle = document.createElement('h1')
+    const headerTemp = document.createElement('span')
+
+    // Add classes
+    headerGroup.className = 'header'
+    headerDate.className = 'date'
+    headerTemp.className = 'temp'
+
+    // Add Children
+    headerGroup.appendChild(headerDate)
+    headerGroup.appendChild(headerTitle)
+    headerGroup.appendChild(headerTemp)
+
+    // Add Content
+    headerDate.textContent = 'MARCH 28, 2020'
+    headerTitle.textContent = 'Lambda Times'
+    headerTemp.textContent = '98°'
+
+    return headerGroup
+
+}
+
+const createHeader = document.querySelector('.header-container')
+createHeader.append(Header())

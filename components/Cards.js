7ccec3e50fd8485ce309de41550bc39a632ebae5
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-09-04 12:01:54 -0400

    Working on Cards.js

--- components/Cards.js
+++ components/Cards.js
@@ -16,6 +16,61 @@
 //     <span>By {author's name}</span>
 //   </div>
 // </div>
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+  .then(response => {
+    console.log('Articles Response:', response)
+
+    const articleInserter = document.querySelector('.cards-container')
+    console.log(articleInserter)
+
+    const articleArray = response.data.articles
+    console.log('Article Array:', articleArray)
+
+    // tabArray.forEach(tab =>{
+    //     const tabElement = document.createElement('div')
+    //     tabElement.className = 'tab'
+    //     tabElement.textContent = tab
+    //     tabInserter.appendChild(tabElement)
+
+  })
+  .catch(error =>{
+    console.log('error:', error)
+  })
+
+function articleMaker(articleObj) {
+
+    // Instatiate elements
+    const articleGroup = document.createElement('div')
+    const articleHeadline = document.createElement('div')
+    const articleAuthor = document.createElement('div')
+    const articleImgContainer = document.createElement('div')
+    const articleImg = document.createElement('img')
+    const articleCredit= document.createElement('span')
+
+    // Add classes
+    articleGroup.className = 'card'
+    articleHeadline.className = 'headline'
+    articleAuthor.className = 'author'
+    articleImgContainer.className = 'img-container'
+
+    // Add Children
+    articleGroup.appendChild(articleHeadline)
+    articleGroup.appendChild(articleAuthor)
+    articleAuthor.appendChild(articleImgContainer)
+    articleImgContainer.appendChild(articleImg)
+
+    // Add Content
+    articleHeadline.textContent = `TestHeadline`
+    //articleImg.src
+    articleCredit.textContent = `By TestAuthor`
+
+    return articleGroup
+
+}
+
+
+
 //
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
